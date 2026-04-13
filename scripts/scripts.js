6ef1530b508c18d690214adad8038398c9c03d9b
HenriqueVenso <henrique_venso@estudante.sesisenai.org.br>
2026-04-13 11:01:20 -0300

    add - Adicionei label que faltava e script

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,7 +2,21 @@ document.getElementById("formadocao").addEventListener("submit", function (e) {
 
     e.preventDefault();
 
+    let nome = document.getElementById("nome").value;
 
+    let email = document.getElementById("email").value;
+
+    let idade = document.getElementById("idade").value;
+
+    let peso = document.getElementById("peso").value;
+
+    let tiposangue = document.getElementById("tiposangue").value;
+
+    let telefone = document.getElementById("telefone").value;
+
+    let cidade = document.getElementById("cidade").value;
+
+    let estado = document.getElementById("estado").value;
 
 
 })
